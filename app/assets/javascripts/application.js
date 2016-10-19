19c9de4f40f2869c825b3139fbf005a8063b21c1
Chris Hanson <chris.hanson@digital.homeoffice.gov.uk>
2016-10-19 16:03:35 +0100

    Initial push
    First prototype for testing.
    
    All cases screen.
    Workflow process.
    Caseworker process.

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,74 @@
+/* global $ */
+/* global GOVUK */
+
+// Warn about using the kit in production
+if (
+  window.sessionStorage && window.sessionStorage.getItem('prototypeWarning') !== 'false' &&
+  window.console && window.console.info
+) {
+  window.console.info('GOV.UK Prototype Kit - do not use for production')
+  window.sessionStorage.setItem('prototypeWarning', true)
+}
+
+$(document).ready(function () {
+  // Use GOV.UK selection-buttons.js to set selected
+  // and focused states for block labels
+  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
+  new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
+
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init()
+
+  // Show and hide toggled content
+  // Where .block-label uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+})
+
+// Make jQuery :contains Case-Insensitive
+// https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
+// NEW selector
+jQuery.expr[':'].Contains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+// OVERWRITES old selecor
+jQuery.expr[':'].contains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+// cases search function
+$('.js-case-search').click(function(e) {
+  e.preventDefault();
+  var search = $('.js-case-search-query').val();
+
+  $('.cases tbody tr').each(function() {
+    $(this).hide();
+  });
+
+  $('.cases tbody tr td:contains("' + search + '")')
+    .parent('tr')
+    .show();
+
+});
+
+// Tabs
+// Left hand tab navigation, e.g. caseworker screen
+
+function tabs(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
---
+++ app/routes.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var router = express.Router()
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+// add your routes here
+
+// Documents check
+router.get('/documents-reject', function (req, res) {
+  var doc1 = req.query.doc1
+  var doc2 = req.query.doc2
+  var doc3 = req.query.doc3
+  var doc4 = req.query.doc4
+
+  if (doc1 == 'doc1' && doc2 == 'doc2' && doc3 == 'doc3' && doc4 == 'doc4') {
+    res.redirect('/fees')
+  } else {
+    res.render('documents-reject')
+  }
+})
+
+
+
+module.exports = router
+
+
