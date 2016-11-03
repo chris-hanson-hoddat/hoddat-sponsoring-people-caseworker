a6328e02a018e5a6cfc0d9ceaaffb6c080d54059
Chris Hanson <chris.hanson@digital.homeoffice.gov.uk>
2016-11-03 11:41:57 +0000

    Updated service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Sponsorship caseworker system',
+  //serviceName: 'Sponsor caseworking',
 
   // Default port that prototype runs on
   port: '3000',
